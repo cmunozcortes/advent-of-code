814f7a057bef2afcf709a2d224ec502f201679da
Chris M <christopher.munoz-cortes@analog.com>
2019-12-16 08:46:13 -0500

    Initial commit for day 2 solution.
    Adding input files and source code.

---
+++ 20191202/program_alarm.py
@@ -0,0 +1,7 @@
+""" Solution to Day 2: 1202 Program Alarm from adventofcode.com """
+
+# Read input file
+with open('20191202/input.txt') as f:
+    for line in f:
+        program = line.strip('\n').split(',')
+        print(program)
