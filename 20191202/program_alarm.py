985779c95e86c152b9043e9352eccb3d8a67a575
Chris M <christopher.munoz-cortes@analog.com>
2019-12-16 15:13:40 -0500

    Adding code for solution to part 2

--- 20191202/program_alarm.py
+++ 20191202/program_alarm.py
@@ -3,42 +3,50 @@
 def run_program(program):
     
     # Initialize pointers
-    opcode_index = 0
-    operand1_index = 1
-    operand2_index = 2
-    result_index = 3
+    instr_pointer = 0
     
     # Execute program while opcode is not
-    while program[opcode_index] != 99:
-        if program[opcode_index] == 1:
-            result = program[program[operand1_index]] + program[program[operand2_index]]
-        elif program[opcode_index] == 2:
-            result = program[program[operand1_index]] * program[program[operand2_index]]
+    while program[instr_pointer] != 99:
+        operand1_pointer = instr_pointer + 1
+        operand2_pointer = instr_pointer + 2
+        result_pointer = instr_pointer + 3
+        
+        # Parse opcode and perform operation (add/multiply)
+        if program[instr_pointer] == 1:
+            result = program[program[operand1_pointer]] + program[program[operand2_pointer]]
+        elif program[instr_pointer] == 2:
+            result = program[program[operand1_pointer]] * program[program[operand2_pointer]]
         else:
             result = None
             print('[ERROR] Invalid opcode. Terminating program.')
             break
-        program[program[result_index]] = result
+
+        # Store the result according to the result pointer
+        program[program[result_pointer]] = result
         
         # Update pointers (increment by 4)
-        opcode_index += 4
-        operand1_index += 4
-        operand2_index += 4
-        result_index += 4
-
-    print('Halting program execution...')
+        instr_pointer += 4
     return
 
 # Read input file
-with open('20191202/input_part1.txt') as f:
+with open('20191202/input.txt') as f:
     for line in f:
         program = list(map(int, line.strip('\n').split(',')))
+
+# Modify input positions 1 and 2
+program[1] = 12
+program[2] = 2
 
 # Execute program
 run_program(program)
 
 # Part 1: 
+print('Part 1 Answer')
 print('First position: ', program[0])
-print('Done.')
 
 # Part 2:
+# Modify operand1 and operand2
+program[1] = 89
+program[2] = 76
+part2_result = 100 * program[1] + program[2]
+print('\nPart 2 Answer: ', part2_result)
