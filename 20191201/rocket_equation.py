6e457d1081a158cfcf9a3142b3694de60b1e849d
cmunoz <cmunozcortes.020@gmail.com>
2020-03-09 21:16:08 -0400

    Cleaned up source code

--- 20191201/rocket_equation.py
+++ 20191201/rocket_equation.py
@@ -17,19 +17,16 @@ def calculate_fuel_for_fuel(fuel):
 
 # Read input file and calculate the fuel required for each module
 # Expect the mass for each module to be in a separate line
-fuel = []
-with open('20191201/input.txt') as f:
-    for line in f:
-        fuel.append(calculate_fuel(int(line)))
+with open('input.txt') as f:
+    fuel = [calculate_fuel(int(line)) for line in f]
 
 # Part 1: Calculate total amount of fuel
 total_amount_fuel = np.sum(fuel)
 print('Total fuel requirement: ', total_amount_fuel)
 
 # Part 2: Same as 1, but include the fuel needed for the fuel
-total_fuel_for_fuel = []
-for fuel_amount in fuel:
-    total_fuel_for_fuel.append(calculate_fuel_for_fuel(fuel_amount))
+total_fuel_for_fuel = [calculate_fuel_for_fuel(fuel_amount) for fuel_amount in
+		       fuel]
 
 total_amount_fuel_for_fuel = np.sum(total_fuel_for_fuel)
 print('Total fuel requirement, including fuel for fuel: ', total_amount_fuel + total_amount_fuel_for_fuel)
